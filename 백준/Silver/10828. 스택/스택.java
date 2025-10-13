2a550ce739fdf2487ba0d9dd1289249105867ae0
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2025-10-13 23:24:59 +0900

    [Silver IV] Title: 스택, Time: 152 ms, Memory: 18816 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,45 +1,54 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
 		Stack<String> stack = new Stack<>();
-		String arr[] = new String[2];
-		int N = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<N; i++) {
-			arr = br.readLine().split(" ");
-			switch(arr[0]){
-				
-				case "push" :
-					stack.add(arr[1]);
+
+		int n = Integer.parseInt(br.readLine());
+
+		for(int i=0; i<n; i++){
+			st = new StringTokenizer(br.readLine());
+			String str = st.nextToken();
+
+			switch (str){
+				case "push":
+					stack.push(st.nextToken());
 					break;
-				case "pop" :
-					if(stack.empty())
+				case "pop":
+					if(stack.empty()){
 						sb.append(-1).append("\n");
-					else
+					} else{
 						sb.append(stack.pop()).append("\n");
+					}
 					break;
-				case "size" :
+				case "size":
 					sb.append(stack.size()).append("\n");
 					break;
-				case "empty" :
-					if(stack.empty())
+				case "empty":
+					if(stack.empty()){
 						sb.append(1).append("\n");
-					else
+					} else{
 						sb.append(0).append("\n");
+					}
 					break;
-				case "top" :
-					if(stack.empty())
+				case "top":
+					if(stack.empty()){
 						sb.append(-1).append("\n");
-					else
+					} else{
 						sb.append(stack.peek()).append("\n");
+					}
 					break;
 			}
 		}
+
 		System.out.println(sb);
 	}
 }
