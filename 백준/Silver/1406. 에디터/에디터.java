77e5c56d3289a8811dc241a2b82046c40fb4f574
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2025-11-22 17:19:03 +0900

    [Silver II] Title: 에디터, Time: 436 ms, Memory: 79648 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -2,53 +2,55 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-		Stack<Character> left_stack = new Stack<>();
-		Stack<Character> right_stack = new Stack<>();
-		
+		StringTokenizer st;
+
 		String str = br.readLine();
-		char arr[] = str.toCharArray();
-		
-		for(int i=0; i<arr.length; i++) {
-			left_stack.push(arr[i]);
+		Stack<Character> lStack = new Stack<>();
+		Stack<Character> rStack = new Stack<>();
+
+		int length = str.length();
+		for(int i=0; i<length; i++){
+			lStack.push(str.charAt(i));
 		}
-		
-		int M = Integer.parseInt(br.readLine());
-		
-		for(int i=0; i<M; i++) {
+
+		int m = Integer.parseInt(br.readLine());
+
+		for(int i=0; i<m; i++){
 			str = br.readLine();
-			arr = str.toCharArray();
-			
-			switch(arr[0]){
-				case 'L' :
-					if(left_stack.empty())
-						break;
-					right_stack.push(left_stack.pop());
+			char []arr = str.toCharArray();
+			switch (arr[0]){
+				case 'L':
+					if(!lStack.isEmpty())
+						rStack.push(lStack.pop());
 					break;
-				case 'D' :
-					if(right_stack.empty())
-						break;
-					left_stack.push(right_stack.pop());
+				case 'D':
+					if(!rStack.isEmpty())
+						lStack.push((rStack.pop()));
 					break;
-				case 'B' :
-					if(left_stack.empty())
-						break;
-					left_stack.pop();
+				case 'B':
+					if(!lStack.empty())
+						lStack.pop();
 					break;
-				case 'P' :
-					left_stack.push(arr[2]);
+				case 'P':
+					lStack.push(Character.valueOf(arr[2]));
+					break;
 			}
 		}
-		while(!left_stack.empty())
-			right_stack.push(left_stack.pop());
-		
-		while(!right_stack.empty())
-			sb.append(right_stack.pop());
-		
+
+		for (Character c : lStack) {
+			sb.append(c);
+		}
+
+		while(!rStack.empty()){
+			sb.append(rStack.pop());
+		}
+
 		System.out.println(sb);
 	}
 }
