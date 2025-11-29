959a9e6d25ca8a7593ade2b50489dcc0d2eb376e
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2025-11-30 00:10:21 +0900

    [Silver IV] Title: 큐, Time: 164 ms, Memory: 19484 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -14,39 +13,41 @@ public class Main {
 
 		int n = Integer.parseInt(br.readLine());
 
-		Deque<Integer> deque = new ArrayDeque<>();
+		Queue<Integer> queue = new ArrayDeque<>();
 
+		int firstNum = 0;
 		for(int i=0; i<n; i++){
 			st = new StringTokenizer(br.readLine());
 
 			switch (st.nextToken()){
 				case "push":
-					deque.offer(Integer.parseInt(st.nextToken()));
+					firstNum = Integer.parseInt(st.nextToken());
+					queue.offer(firstNum);
 					break;
 				case  "pop":
-					if(!deque.isEmpty())
-						sb.append(deque.poll()).append("\n");
+					if(!queue.isEmpty())
+						sb.append(queue.poll()).append("\n");
 					else
 						sb.append(-1).append("\n");
 					break;
 				case "size":
-					sb.append(deque.size()).append("\n");
+					sb.append(queue.size()).append("\n");
 					break;
 				case "empty":
-					if(deque.isEmpty())
+					if(queue.isEmpty())
 						sb.append(1).append("\n");
 					else
 						sb.append(0).append("\n");
 					break;
 				case "front":
-					if(!deque.isEmpty())
-						sb.append(deque.peek()).append("\n");
+					if(!queue.isEmpty())
+						sb.append(queue.peek()).append("\n");
 					else
 						sb.append(-1).append("\n");
 					break;
 				case "back":
-					if(!deque.isEmpty())
-						sb.append(deque.peekLast()).append("\n");
+					if(!queue.isEmpty())
+						sb.append(firstNum).append("\n");
 					else
 						sb.append(-1).append("\n");
 					break;
