76babbdbfdf30ab113063571513f69731b0593c7
heeyeon <111184269+heeyeon3050@users.noreply.github.com>
2025-12-22 22:22:11 +0900

    [Silver III] Title: 후위 표기식2, Time: 108 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.java
@@ -1,41 +1,41 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int n = Integer.parseInt(br.readLine());
+		double arr[] = new double[n];
+
 		String str = br.readLine();
 
-		Map<Character, Double> map = new HashMap<>();
-		for(int i=0; i<n; i++){
-			double num = Integer.parseInt(br.readLine());
-			map.put((char)('A'+i), num);
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
 
 		Stack<Double> stack = new Stack<>();
-
-		int length = str.length();
-		for(int i=0; i<length; i++){
-			char x = str.charAt(i);
-			if(x >= 'A' && x <= 'Z'){
-				stack.add(map.get(x));
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+				stack.push(arr[str.charAt(i) - 'A']);
 			} else {
 				double a = stack.pop();
 				double b = stack.pop();
-				switch (x){
+
+				switch (str.charAt(i)) {
+					case '*':
+						stack.push(b * a);
+						break;
 					case '+':
-						stack.push(b+a);
+						stack.push(b + a);
 						break;
 					case '-':
-						stack.push(b-a);
-						break;
-					case '*':
-						stack.push(b*a);
+						stack.push(b - a);
 						break;
 					case '/':
-						stack.push(b/a);
+						stack.push(b / a);
 						break;
 				}
 			}
